7c3d0bc30f81aa28776b89062f7853a4a159b41b
Okan <okany4404@gmail.com>
2024-03-13 22:40:44 +0300

    NotFound page added

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import './NotFound.css'
 
 export default function NotFound() {
-	return <div>NotFound</div>;
+	return (
+
+		<div className="wrapper">
+			<div className="container-404">
+				<div id="scene" className="scene" data-hover-only="false">
+					<div className="two" data-depth="0.60">
+						<div className="content">
+							<span className="piece"></span>
+							<span className="piece"></span>
+							<span className="piece"></span>
+						</div>
+					</div>
+					<div className="three" data-depth="0.40">
+						<div className="content">
+							<span className="piece"></span>
+							<span className="piece"></span>
+							<span className="piece"></span>
+						</div>
+					</div>
+					<p className="p404" data-depth="0.50">404</p>
+					<p className="p404" data-depth="0.10">404</p>
+				</div>
+				<div className="text">
+					<article>
+						<p>Sayfa Bulunamadı</p>
+					</article>
+				</div>
+			</div>
+		</div>
+	)
 }
